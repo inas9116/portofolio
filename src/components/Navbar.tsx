b8e4c3a0518d6974cf1885667bd80fc6f1476283
inas9116 <inas9116@gmail.com>
2024-10-26 14:14:51 +0700

    navabr1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
+
+import image from '../assets/NNlogo.avif'
+
 const Navbar = () => {
   return (
     <div className="  bg-cyan-500 flex justify-between items-center pl-4">
       <div>
-        <img src="src/assets/NNlogo.avif"
-        className="w-[50px] h-[50px]"/>
+        <img src= {image} className="w-[50px] h-[50px]"/>
       </div>
       <div className=" max-w-full container flex items-center justify-end gap-5 p-5 text-gray-50">
         <button className="max-w-[600px] rounded-md border px-10 hover:bg-sky-700 ...">
